ef3a4e2ffd598a684815cf8e0111600b98f661ad
Greg Berezny <gb5426@mun.ca>
2019-03-17 20:02:05 -0400

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+//var messages = require('./lib/message.js');
+
+var app = express();
+
+// set up handlebars view engine
+var handlebars = require('express-handlebars')
+    .create({ defaultLayout:'main' });
+    
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('menu');
+});
+
+app.get('/game', function(req, res) {
+	res.render('game');
+});
+
+app.listen(app.get('port'));
